a23cb0f086d8a315d85bd9bf1323d2b93d5b5487
Vladyslavi <135136418+Vladyslavi@users.noreply.github.com>
2024-01-23 22:08:16 +0200

    commit

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -10,10 +10,6 @@ class Component<T> {
   constructor(public props: T) {}
 }
 
-interface PageProps extends ComponentProps {
-  title: string;
-}
-
 class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
